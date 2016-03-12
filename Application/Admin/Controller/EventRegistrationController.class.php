d91b6cb581ad35a0d13eb5ff4d44a645c27fdf46
lijianzwm <lijian8931@163.com>
2016-03-12 17:33:36 +0800

    添加活动报名Controller

---
+++ Application/Admin/Controller/EventRegistrationController.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 2016/3/12
+ * Time: 16:38
+ */
+
+namespace Admin\Controller;
+
+
+/**
+ * Class EventRegistrationController
+ * @package Admin\Controller
+ * 活动报名
+ */
+class EventRegistrationController extends CommonController{
+    public function editEventRegistration(){
+        //TODO 编辑报名事件
+    }
+
+    public function editEventRegistrationHandler(){
+        //TODO 保存报名事件
+    }
+}
