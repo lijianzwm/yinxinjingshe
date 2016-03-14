230565395cb54ae62073a1f5429ce9d5c47bb5ae
lijianzwm <lijian8931@163.com>
2016-03-14 20:29:00 +0800

    添加报名活动推送功能

--- Application/Admin/Controller/ActivityController.class.php
+++ Application/Admin/Controller/ActivityController.class.php
@@ -73,4 +73,71 @@ class ActivityController extends CommonController{
         echo json_encode($json);
     }
 
+    /**
+     * 推送报名活动
+     */
+    public function pushActivity(){
+        $activityId = I("id");
+        $activityModel = M("activity");
+        $newsModel = M("news");
+        $activity = $activityModel->where("id='$activityId'")->find();
+        if( $activity["news_id"] != 0 ){//如果这个消息被推送过了
+            $this->error("这个消息已经被推送过了！");
+        }
+        $news["title"] = $activity["title"];
+        $news["content"] = $activity["content"];
+        $news["img_name"] = $activity["img_name"];
+        $news["author"] = $activity["sponsor"];
+        $news["abstract"] = $activity["activity_name"];
+        $activityModel->startTrans();//开启事务
+        $newsId = $newsModel->add($news);
+        if( $newsId ){
+            $news["id"] = $newsId;
+            $activity['is_pushed'] = C("ACTIVITY_PUSHED");
+            $activity['news_id'] = $newsId;
+            if( $activityModel->save($activity) ){
+                $activityModel->commit();
+                $this->assign("news", $news);
+                $this->display();
+            }else{
+                $activityModel->rollback();
+                $this->error("设置is_pushed失败！");
+            }
+        }else{
+            $activityModel->rollback();
+            $this->error("添加推送失败！");
+        }
+    }
+
+    /**
+     * 删除已经推送的报名活动
+     */
+    public function deletePushedActivity(){
+        $activityId = I("id");
+        $activityModel = M("activity");
+        $newsModel = M("news");
+        $activity = $activityModel->where("id='$activityId'")->find();
+        if( !$activity ){
+            $this->error("查无此活动！");
+        }
+        $newsId = $activity['news_id'];
+        if( $newsId == 0 ){//如果报名活动未被推送
+            $this->error("无法取消未被推送的活动！");
+        }
+        $news = $newsModel->where("id='$newsId'")->find();
+        if( !$news ){
+            $this->error("查无此推送！");
+        }
+        $activity['is_pushed'] = 0;
+        $activity["news_id"] = 0;
+        $newsModel->startTrans();
+        if( $newsModel->where("id='$newsId'")->delete() && $activityModel->save($activity)){
+            $newsModel->commit();
+            $this->success("取消推送成功！", U("activityList"));
+        }else{
+            $newsModel->rollback();
+            $this->error("取消推送失败！");
+        }
+    }
+
 }
