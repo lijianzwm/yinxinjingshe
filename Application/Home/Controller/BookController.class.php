440d5265155e1e3658e8c4c236f26283d27ec463
lijianzwm <lijian8931@163.com>
2016-04-09 17:58:10 +0800

    添加BookController

---
+++ Application/Home/Controller/BookController.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 2016/4/9
+ * Time: 17:52
+ */
+
+namespace Home\Controller;
+
+
+use Think\Controller;
+
+class BookController extends Controller{
+
+}
